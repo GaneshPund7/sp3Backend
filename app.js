216cfb3c867534c9adbde50a39900749b486106e
Ganesh <ganeshpund0000@gmail.com>
2024-01-25 23:27:39 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-var routes = require('./router/router');
+const routes = require('./router/router');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config(); 
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,21 +14,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
 
-mongoose.connect('mongodb+srv://ganeshpund0000:H37rsi1YsFng3fhS@db-sp3.wn47nsb.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 const taskRoutes = require('./router/router');
 app.use('/api', taskRoutes);
 const { connectToDatabase, getClient } = require('./db');
 
 async function startServer() {
   await connectToDatabase();
- 
-
   const dbClient = getClient();
-  
 }
 
 startServer();
